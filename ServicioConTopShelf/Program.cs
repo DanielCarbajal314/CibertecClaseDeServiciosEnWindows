6d37fccf96c74464253b327a8e88939ab68338af
DanielCarbajal314 <daniel.carbajal@pucp.edu.pe>
2018-01-21 09:38:31 -0500

    First Commit

---
+++ ServicioConTopShelf/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace ServicioConTopShelf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(configuracionDelServicio=> {
+                configuracionDelServicio.Service<ServicioConvertidorDeArchivos>
+                (
+                    instanciaDelServicio =>
+                    {
+                        instanciaDelServicio.ConstructUsing(() => new ServicioConvertidorDeArchivos());
+                        instanciaDelServicio.WhenStarted(servicioEnEjecucion => servicioEnEjecucion.Comenzar());
+                        instanciaDelServicio.WhenStopped(servicioEnEjecucion => servicioEnEjecucion.Detenerce());
+                    }
+                );
+                configuracionDelServicio.StartAutomatically();
+                configuracionDelServicio.SetDisplayName("Conversor de Archivos");
+                configuracionDelServicio.SetDescription("Convierte los archivos en la carpeta temporal a mayusculas");
+                configuracionDelServicio.SetServiceName("ConversorDeArchivos");
+            });
+        }
+    }
+}
---
+++ ServicioConTopShelf/ServicioConvertidorDeArchivos.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServicioConTopShelf
+{
+    class ServicioConvertidorDeArchivos
+    {
+        private FileSystemWatcher _observadorDeArchivos;
+        public bool Comenzar() {
+            this._observadorDeArchivos = new FileSystemWatcher(@"C:\temp\archivosDeTexto", "*.txt");
+            this._observadorDeArchivos.Created += this.convertirArchivo;
+            this._observadorDeArchivos.IncludeSubdirectories = false;
+            this._observadorDeArchivos.EnableRaisingEvents = true;
+            return true;
+        }
+
+        private void convertirArchivo(object sender, FileSystemEventArgs e)
+        {
+            string contenidoDelArchivo = File.ReadAllText(e.FullPath);
+            if (e.FullPath.Contains("convertido")) return;
+            string contenideoDelArchivoEnMayuscula = contenidoDelArchivo.ToUpperInvariant();
+            string ruteDelNuevoArchivo = this.enrutarArchivo(e);
+            File.WriteAllText(ruteDelNuevoArchivo, contenideoDelArchivoEnMayuscula);
+
+        }
+
+        private string enrutarArchivo(FileSystemEventArgs e)
+        {
+            string directorio = Path.GetDirectoryName(e.FullPath);
+            string nombreDelArchivo = Path.GetFileName(e.FullPath);
+            string nombreDelNuevoArchivo = nombreDelArchivo.Replace(".txt","") + ".convertido.txt";
+            return Path.Combine(directorio, nombreDelNuevoArchivo);
+        }
+
+        public bool Detenerce() {
+            this._observadorDeArchivos.Dispose();
+            return true;
+        }
+
+    }
+}
